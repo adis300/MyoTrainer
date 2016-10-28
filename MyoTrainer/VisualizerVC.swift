099eb83f86832ff1481490caac93c1e6ce56c8f6
Disi A <adis@live.cn>
2016-10-27 21:39:46 -0400

    Initial commit before refactoring pose types.

---
+++ MyoTrainer/VisualizerVC.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MyoTrainer
+//
+//  Created by Innovation on 10/27/16.
+//  Copyright © 2016 Votebin. All rights reserved.
+//
+
+import Cocoa
+
+class VisualizerVC: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
