595f734b3ff6c3ad12df4f86f768851e3eb9035f
Disi A <adis@live.cn>
2016-10-29 10:52:56 -0400

    Visualizer barchart implementation.

---
+++ MyoTrainer/Theme/AppTheme.swift
@@ -0,0 +1,46 @@
+//
+//  Theme.swift
+//  MyoTrainer
+//
+//  Created by Disi A on 10/28/16.
+//  Copyright © 2016 Votebin. All rights reserved.
+//
+
+import Cocoa
+
+class AppTheme{
+    
+    static let BAR_COLORS = [
+        
+        // Redish colors
+        NSColor(calibratedRed: 244/255, green: 67/255, blue: 54/255, alpha: 1), // Red rgb(244, 67, 54)
+        // UIColor(red: 233/255, green: 30/255, blue: 99/255, alpha: 1), // Pink rgb(233, 30, 99)
+        // UIColor(red: 156/255, green: 39/255, blue: 176/255, alpha: 1),  //Purple rgb(156, 39, 176)
+        NSColor(calibratedRed: 103/255, green: 58/255, blue: 183/255, alpha: 1),   // Deep purple rgb(103, 58, 183)
+        
+        // Blueish colors
+        // UIColor(red: 63/255, green: 81/255, blue: 181/255, alpha: 1), // Indigo rgb(63, 81, 181)
+        // UIColor(red: 33/255, green: 150/255, blue: 243/255, alpha: 1), // Blue rgb(33, 150, 243)
+        NSColor(calibratedRed: 3/255, green: 169/255, blue: 244/255, alpha: 1),  // Light Blue rgb(3, 169, 244)
+        // UIColor(red: 0/255, green: 188/255, blue: 212/255, alpha: 1),  //Cyan rgb(0, 188, 212)
+        
+        // Greenish colors
+        NSColor(calibratedRed: 0/255, green: 150/255, blue: 136/255, alpha: 1),  // Teal rgb(0, 150, 136)
+        // UIColor(red: 76/255, green: 175/255, blue: 80/255, alpha: 1), // Green rgb(76, 175, 80)
+        NSColor(calibratedRed: 139/255, green: 195/255, blue: 74/255, alpha: 1),  // Light green rgb(139, 195, 74)
+        // UIColor(red: 205/255, green: 220/255, blue: 57/255, alpha: 1), // Lime rgb(205, 220, 57)
+        
+        // Yellowish colors
+        NSColor(calibratedRed: 1, green: 235/255, blue: 59/255, alpha: 1), // Yellow rgb(255, 235, 59)
+        // UIColor(red: 1, green: 193/255, blue: 7/255, alpha: 1),   // Amber rgb(255, 193, 7)
+        NSColor(calibratedRed: 1, green: 152/255, blue: 0/255, alpha: 1),  // Orange rgb(255, 152, 0)
+        // UIColor(red: 1, green: 87/255, blue: 34/255, alpha: 1), // DeepOrange rgb(255, 87, 34)
+        NSColor(calibratedRed: 121/255, green: 85/255, blue: 72/255, alpha: 1), // Brown rgb(121, 85, 72)
+        // UIColor(red: 158/255, green: 158/255, blue: 158/255, alpha: 1),  //Grey rgb(158, 158, 158)
+        NSColor(calibratedRed: 96/255, green: 125/255, blue: 139/255, alpha: 1),      // BlueGrey rgb(96, 125, 139)
+    ]
+    
+    static let BAR_COLOR = NSColor(calibratedRed: 63/255, green: 81/255, blue: 181/255, alpha: 1) // Indigo rgb(63, 81, 181)
+
+    
+}
--- MyoTrainer/VisualizerVC.swift
+++ MyoTrainer/VisualizerVC.swift
@@ -7,31 +7,88 @@
 //
 
 import Cocoa
+import Charts
 
 class VisualizerVC: NSViewController, MyoDelegate{
 
     var myo:Myo = Myo.init(appIdentifier: "com.votebin.brainco", updateTime: 50)  // Blocking UI update every 50ms
+    var fileOutputStream:OutputStream?
+    
+    let skipper = 20
+    var skipperCounter = 0
     
     @IBOutlet weak var connectButton: NSButton!
     @IBOutlet weak var statusLabel: NSTextField!
-    
+    @IBOutlet weak var writeToFileCheckBox: NSButton!
+    @IBOutlet weak var filePathField: NSTextField!
+    @IBOutlet weak var lineChart0: LineChartView!
+    @IBOutlet weak var barChart: BarChartView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         myo.delegate = self
-        // myo = (NSApplication.shared().delegate as! AppDelegate).myo
-        // Do any additional setup after loading the view.
+        setUpVisulization()
     }
     /*
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
-    }*/
+    }
+    */
 
     @IBAction func connectClick(_ sender: AnyObject) {
+        // Read teh file path
+        if(filePathField.stringValue.characters.count > 0){
+            fileOutputStream = OutputStream(toFileAtPath: filePathField.stringValue, append: true)
+            fileOutputStream?.open()
+        }
         myo.connectWaiting(3000)
         myo.startUpdate();
+    }
+    @IBAction func stopClick(_ sender: AnyObject) {
+        myo.stopUpdate()
+        fileOutputStream?.close()
+    }
+    
+    // Data properties
+    let emgMagnitudeData = BarChartData()
+    let barDataSerie = BarChartDataSet(values: ([0,0,0,0,0,0,0,0].enumerated().map { x, y  in return BarChartDataEntry(x: Double(x), y: Double(y))}), label: "EMG Magnitude")
+
+    func setUpVisulization(){
+        
+        barDataSerie.colors = [AppTheme.BAR_COLOR]
+        emgMagnitudeData.addDataSet(barDataSerie)
+        
+        barChart.data = emgMagnitudeData
+        barChart.gridBackgroundColor = NSUIColor.white
+        barChart.chartDescription?.text = ""
+
+    }
+    
+    // Utility function implementation
+    
+    func writeEmgToFile(emgData: [Int8], timestamp: UInt64){
+        let line = "\(emgData[0]),\(emgData[1]),\(emgData[2]),\(emgData[3]),\(emgData[4]),\(emgData[5]),\(emgData[6]),\(emgData[7]),\(timestamp)\n"
+        fileOutputStream?.write(line, maxLength: line.lengthOfBytes(using: String.Encoding.utf8))
+    }
+    
+    func plotEmgMagnitudeIndicator(emgData: [Int8]){
+        skipperCounter += 1
+        if (skipperCounter == skipper){
+            skipperCounter = 0
+            for (index, magnitude) in emgData.enumerated() {
+                // Use ABS after double
+                barDataSerie.entryForIndex(index)?.y = abs(Double(magnitude))
+            }
+            emgMagnitudeData.notifyDataChanged()
+            barChart.notifyDataSetChanged()
+        }
+        
+    }
+    
+    func plotEmgSignalLineChart(emgData:[Int8]){
+        
     }
     
     // Myo delegate methods
@@ -47,9 +104,15 @@ class VisualizerVC: NSViewController, MyoDelegate{
     }
     
     func myo(_ myo: Myo!, onEmgData emgData: UnsafeMutablePointer<Int8>!, timestamp: UInt64) {
-        let arrayData = Array(UnsafeBufferPointer(start: emgData, count: 8))
-        print(arrayData)
+        let arrayEmgData = Array(UnsafeBufferPointer(start: emgData, count: 8))
+        // Before applying filter
+        if(writeToFileCheckBox.state == NSOnState){
+            writeEmgToFile(emgData: arrayEmgData, timestamp: timestamp)
+        }
+        plotEmgMagnitudeIndicator(emgData: arrayEmgData)
+        // print(arrayEmgData)
     }
+    
     
 }
 
